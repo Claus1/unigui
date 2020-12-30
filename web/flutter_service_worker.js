1c17d34a3174280f51199dedb6f8564c4b8b6a9e
George Dernovoy <g.dernovoy@gmail.com>
2020-12-30 11:50:33 +0700

    u web

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -26,10 +26,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "c0475af92ab1a99b939db0454994631b",
+"main.dart.js": "2c89e954bc7583943e83072fe3168b49",
 "manifest.json": "e16fc65a08b927514710c67815a12d24",
-"index.html": "ae884f6a3917eea31af1deda4075a280",
-"/": "ae884f6a3917eea31af1deda4075a280"
+"index.html": "bfac93443162c33a6f3a770220f85970",
+"/": "bfac93443162c33a6f3a770220f85970"
 };
 
 // The application shell files that are downloaded before a service worker can
