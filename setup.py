66c946f84d652d7223824c315dc4d275b51a16b9
Georgii Dernovyi <g.dernovoy@gmail.com>
2023-03-13 18:00:57 +0700

    graph 0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name='unigui',
-      version='1.3.0',      
+      version='1.3.1',      
       license='MIT',
       author='Georgii Dernovyi',
       author_email='g.dernovoy@gmail.com',
