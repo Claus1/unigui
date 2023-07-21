e58160a7c1627361b448147aacab89e6f51785d0
George Dernovoy <g.dernovoy@gmail.com>
2023-07-21 18:08:59 +0700

    web 1.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name='unigui',
-      version='1.8.5',      
+      version='1.9.0',      
       license='MIT',
       author='Georgii Dernovyi',
       author_email='g.dernovoy@gmail.com',
